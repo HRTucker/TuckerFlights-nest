3eea995c2fdbda81ec6a88fca722ad79e73d174f
Hazhord <hazhord@googlemail.com>
2023-03-20 14:49:34 +0000

    Setup CRUD methods and RESTful connections

--- src/flights/flights.controller.ts
+++ src/flights/flights.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Flight } from './flight.model';
 import { Flights } from './flights.entity';
 import { FlightsService } from './flights.service';
 
@@ -6,13 +7,41 @@ import { FlightsService } from './flights.service';
 export class FlightsController {
     constructor(private readonly flightService: FlightsService) {}
 
+    // CREATE
+    @Post()
+    async create(@Body() flight: Flight): Promise<Flights[]> {
+        return this.flightService.create(flight);
+    }
+
+    //Read All
     @Get()
     findAll(): Promise<Flights[]> {
         return this.flightService.findAll();
     }
 
-    @Get("/:id")
-    findOne(@Param() param): Promise<Flights[]> {
+    // QUERY
+    @Get("query/:orig/:dest")
+    async query(@Param('orig') orig, @Param('dest') dest): Promise<any> {
+        return this.flightService.query(orig, dest);
+    }
+
+    // READ ONE
+    @Get(":id")
+    async findOne(@Param() param): Promise<Flight> {
         return this.flightService.findOne(param.id);
+    }
+
+    // UPDATE
+    @Patch(":id/update")
+    async update(@Param('id') id, @Body() flight: Flight): Promise<any> {
+        flight.id = Number(id);
+        return this.flightService.update(flight);
+    }
+
+
+    // DELETE
+    @Delete(":id/delete")
+    async delete(@Param('id') id): Promise<any> {
+        return this.flightService.delete(id);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+    await app.listen(3000);
 }
 bootstrap();
